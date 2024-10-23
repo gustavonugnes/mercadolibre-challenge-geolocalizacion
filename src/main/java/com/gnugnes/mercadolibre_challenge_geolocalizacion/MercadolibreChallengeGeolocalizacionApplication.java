913d7a965806fff9d6c774d3593b65a5985b757c
Gustavo Nugnes <gustavo.nugnes@gmail.com>
2024-10-23 19:56:54 -0300

    Commit inicial

---
+++ src/main/java/com/gnugnes/mercadolibre_challenge_geolocalizacion/MercadolibreChallengeGeolocalizacionApplication.java
@@ -0,0 +1,13 @@
+package com.gnugnes.mercadolibre_challenge_geolocalizacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadolibreChallengeGeolocalizacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadolibreChallengeGeolocalizacionApplication.class, args);
+	}
+
+}
