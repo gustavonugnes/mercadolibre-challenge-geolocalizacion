a960eddd11a67df3a877d261a6856c368466dc6d
Gustavo Nugnes <gustavo.nugnes@gmail.com>
2024-10-25 20:12:25 -0300

    Add redis cache

--- src/main/java/com/gnugnes/mercadolibre_challenge_geolocalizacion/App.java
+++ src/main/java/com/gnugnes/mercadolibre_challenge_geolocalizacion/App.java
@@ -4,9 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @Slf4j
 @SpringBootApplication
+@EnableCaching
 public class App implements CommandLineRunner {
 
 	public static void main(String[] args) {
--- src/main/java/com/gnugnes/mercadolibre_challenge_geolocalizacion/services/StatisticsService.java
+++ src/main/java/com/gnugnes/mercadolibre_challenge_geolocalizacion/services/StatisticsService.java
@@ -2,6 +2,7 @@ package com.gnugnes.mercadolibre_challenge_geolocalizacion.services;
 
 import com.gnugnes.mercadolibre_challenge_geolocalizacion.repositories.InvocationRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,6 +19,7 @@ public class StatisticsService {
         return invocationRepository.findMinDistance();
     }
 
+    @Cacheable(value = "myCache")
     public Double getAverageDistance() {
         return invocationRepository.findAverageDistance();
     }
