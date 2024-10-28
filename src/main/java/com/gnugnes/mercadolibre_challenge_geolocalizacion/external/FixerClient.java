257fb727935597fe50ee047d64972e0dd8cb164a
Gustavo Nugnes <gustavo.nugnes@gmail.com>
2024-10-28 18:02:03 -0300

    Improvements to external clients' code

--- src/main/java/com/gnugnes/mercadolibre_challenge_geolocalizacion/external/FixerClient.java
+++ src/main/java/com/gnugnes/mercadolibre_challenge_geolocalizacion/external/FixerClient.java
@@ -4,19 +4,26 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gnugnes.mercadolibre_challenge_geolocalizacion.dtos.ExchangeRatesDto;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.io.IOException;
-import java.io.InputStream;
+
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Slf4j
 @Component
 public class FixerClient {
 
     private static final String BASE_URL = "http://data.fixer.io/api";
+
+    /**
+     * Hardcoded access key for external service. This is only used for testing purposes
+     * and not intended for production use. In a real application, the access key should
+     * be retrieved from a secure source, such as environment variables, a secure vault,
+     * or a configuration management system, to prevent exposure and enhance security.
+     */
     private static final String ACCESS_KEY = "f76e73194322bee88b5cd3e760727ffd";
 
     private final WebClient webClient;
@@ -27,7 +34,7 @@ public class FixerClient {
 
         this.webClient = WebClient.builder()
                 .baseUrl(BASE_URL)
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
                 .build();
     }
 
@@ -43,10 +50,17 @@ public class FixerClient {
                 .block();
     }
 
-    public ExchangeRatesDto getExchangeRatesFake() {
+    /**
+     * Mock method to simulate the retrieval of exchange rates data, bypassing an actual call
+     * to the external Fixer API (https://fixer.io/). This is used to avoid consuming limited
+     * API requests during testing or development. Instead, it reads from a local JSON file
+     * (`fixer_exchange_rates.json`) that contains sample data formatted as an ExchangeRatesDto.
+     */
+    public ExchangeRatesDto getExchangeRatesMock() {
         TypeReference<ExchangeRatesDto> typeReference = new TypeReference<>() {
         };
-        InputStream inputStream = TypeReference.class.getResourceAsStream("/json/exchange_rates.json");
+        var inputStream = TypeReference.class
+                .getResourceAsStream("/json/mocks/fixer_exchange_rates.json");
         try {
             return objectMapper.readValue(inputStream, typeReference);
         } catch (IOException e) {
--- src/main/java/com/gnugnes/mercadolibre_challenge_geolocalizacion/external/Ip2CountryClient.java
+++ src/main/java/com/gnugnes/mercadolibre_challenge_geolocalizacion/external/Ip2CountryClient.java
@@ -4,20 +4,26 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gnugnes.mercadolibre_challenge_geolocalizacion.dtos.Ip2CountryDto;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.io.IOException;
-import java.io.InputStream;
+
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Slf4j
 @Component
-//@RequiredArgsConstructor
 public class Ip2CountryClient {
 
     private static final String BASE_URL = "http://api.ipapi.com";
+
+    /**
+     * Hardcoded access key for external service. This is only used for testing purposes
+     * and not intended for production use. In a real application, the access key should
+     * be retrieved from a secure source, such as environment variables, a secure vault,
+     * or a configuration management system, to prevent exposure and enhance security.
+     */
     private static final String ACCESS_KEY = "20580b936a8be84b532774cf2f119ea6";
 
     private final WebClient webClient;
@@ -28,20 +34,8 @@ public class Ip2CountryClient {
 
         this.webClient = WebClient.builder()
                 .baseUrl(BASE_URL)
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
                 .build();
-    }
-
-    public Ip2CountryDto getCountryDataFake(String ip) {
-        TypeReference<Ip2CountryDto> typeReference = new TypeReference<>() {
-        };
-        InputStream inputStream = TypeReference.class.getResourceAsStream("/json/country_ar.json");
-        try {
-            return objectMapper.readValue(inputStream, typeReference);
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-        return null;
     }
 
     public Ip2CountryDto getCountryData(String ip) {
@@ -57,4 +51,21 @@ public class Ip2CountryClient {
                 .block();
     }
 
+    /**
+     * Mock method to simulate retrieving country data based on an IP address, bypassing an actual call
+     * to the external IP API (https://ipapi.com/). This method is used to avoid consuming limited
+     * API requests during testing or development. Instead, it reads from a local JSON file
+     * (`ip2country_3.json`) that contains sample data formatted as an Ip2CountryDto.
+     */
+    public Ip2CountryDto getCountryDataMock(String ip) {
+        TypeReference<Ip2CountryDto> typeReference = new TypeReference<>() {
+        };
+        var inputStream = TypeReference.class.getResourceAsStream("/json/mocks/ip2country_3.json");
+        try {
+            return objectMapper.readValue(inputStream, typeReference);
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+        return null;
+    }
 }
